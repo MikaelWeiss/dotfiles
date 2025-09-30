82500c775802fa54fca969dff09d08c11ebdd7bc
Mikael Weiss <campingmikael@icloud.com>
2025-09-30 15:23:38 -0600

    Updated for command + r

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,27 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write" })
 vim.keymap.set("n", "<leader>W", ":wq<CR>", { desc = "Write and quit" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>Q", ":q!<CR>", { desc = "Quit without saving" })
+
+-- Run code based on file type with cmd+r
+vim.keymap.set("n", "<D-r>", function()
+  local filetype = vim.bo.filetype
+  local file = vim.fn.expand("%:p")
+  local cmd
+
+  if filetype == "elixir" then
+    cmd = "elixir " .. vim.fn.shellescape(file)
+  elseif filetype == "python" then
+    cmd = "python3 " .. vim.fn.shellescape(file)
+  elseif filetype == "swift" then
+    cmd = "swift " .. vim.fn.shellescape(file)
+  else
+    print("No run command configured for filetype: " .. filetype)
+    return
+  end
+
+  Snacks.terminal(cmd, {
+    win = { position = "bottom" },
+    interactive = true,
+    auto_close = false
+  })
+end, { desc = "Run current file" })
