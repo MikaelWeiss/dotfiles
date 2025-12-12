8d9026c7792f4c63cc4f508d748795d7fc30e372
Mikael Weiss <campingmikael@icloud.com>
2025-12-12 05:07:26 -0700

    Updated flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,15 +2,22 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
   };
 
-  outputs = { self, nixpkgs, quadlet-nix }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, quadlet-nix }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
       modules = [
-        ./configuration.nix  # Now in same directory
+        ./configuration.nix
         quadlet-nix.nixosModules.quadlet
       ];
     };
