20ab066c2ba50adfaa5932bd5314a2e5a3d00048
Mikael Weiss <campingmikael@icloud.com>
2025-12-12 05:05:01 -0700

    Moved things around

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,15 +1,17 @@
 {
+  description = "NixOS configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
   };
 
-  outputs = { nixpkgs, quadlet-nix, ... }: {
-    nixosConfigurations.yourHostname = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, quadlet-nix }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
-          quadlet-nix.nixosModules.quadlet
+        ./configuration.nix  # Now in same directory
+        quadlet-nix.nixosModules.quadlet
       ];
     };
   };
